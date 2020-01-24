1dbd7d18d063c7638d47e144f26347588c361c4e
Le5ya <49882766+Le5ya@users.noreply.github.com>
2020-01-24 21:47:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+var mySviper = new Swiper('.swiper-container',{
+	pagination: {
+		el: '.swiper-pagination',
+		bulletClass:'projects-bullet',
+		bulletActiveClass:'projects-bullet-active',
+		clickable: true
+	},
+});
